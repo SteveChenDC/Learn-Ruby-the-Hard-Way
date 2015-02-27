1c8fa5182f04449ec1737dfc523757101e3d9e3c
Steve Chen <steven20904@yahoo.com>
2015-02-27 11:53:57 -0500

    Add ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,30 @@
+# Prints string below
+puts "Mary had a little lamb."
+# Prints string below. #{'snow'} is not a variable. It's just a string because of the ''
+puts "Its fleece was white as #{'snow'}."
+# Prints string below
+puts "And everywhere that Mary went."
+# Prints . 10 times on one line
+puts "." * 10 # what'd that do?
+
+# Declares all these variables 
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+# watch that print vs. puts on this line what's it do?
+# Prints "Cheese" from the end variables
+print end1 + end2 + end3 + end4 + end5 + end6
+# Prints string on the same line as above
+puts end7 + end8 + end9 + end10 + end11 + end12
+
